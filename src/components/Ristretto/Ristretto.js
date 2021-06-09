1ee8efaf20e1c4253527f75340b504c0c8b85fe7
Florence Anipa <mawuaniipa@gmail.com>
2021-06-09 11:35:29 +0300

    Added SASS

--- src/components/Ristretto/Ristretto.js
+++ src/components/Ristretto/Ristretto.js
@@ -1,8 +1,20 @@
 import React from "react";
+// import axios from "axios";
+import "./Ristretto.scss";
 
 function Ristretto() {
+//   const getRistrettoCoffee = async () => {
+//     try {
+//       const data = await axios.get("https://darkroastedbeans.coffeeit.nl/coffee-machine/60ba0ad8493d9eac4811a9a6");
+//       console.log("Coffee", data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+  //   getRistrettoCoffee();
   return (
-    <div>
+    <div className>
       <p>Ristretto</p>
     </div>
 
